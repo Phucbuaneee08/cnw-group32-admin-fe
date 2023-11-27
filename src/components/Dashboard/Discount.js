08c5c88defbd5c524e7bc79c8e60c1cdad1c1f8b
Phucbuaneee08 <phucbua27082002@gmail.com>
2023-11-27 22:18:40 +0700

    add discount table on discount page

--- src/components/Dashboard/Discount.js
+++ src/components/Dashboard/Discount.js
@@ -27,40 +27,40 @@ function Discount() {
             console.log(err)
         })
     }, [reload])
-    // return (
-    //     <div>
-    //         <div>
-    //             <button
-    //                 className="
-    //                 mx-2 my-2 px-3 py-2
-    //                 border-2 border-gray-200 
-    //                 rounded-lg
-    //                 text-sm
-    //                 transition duration-150 ease-in-out 
-    //                 hover:border-gray-300"
-    //                 onClick={() => setIsCreate(true)}
-    //             >
-    //                 + Thêm Discount
-    //             </button>
-    //             <ModalCreate openCreate={[isCreate, setIsCreate]} reload={[reload, setReload]} />
-    //         </div>
-    //         {isLoading ? (
-    //             <div className="flex justify-center mt-6">
-    //                 <div
-    //                     className="w-16 h-16 border-8 border-green-400 rounded-full border-solid animate-spin"
-    //                     style={{ borderTop: "8px solid transparent" }}
-    //                 />
-    //             </div>
-    //         ) : (
-    //             <div className='pb-10'>
-    //                 <DiscountTable
-    //                     discountProps={[discount, setDiscount]}
-    //                     reload={[reload, setReload]}
-    //                 />
-    //             </div>
-    //         )}
-    //     </div>
-    // )
+    return (
+        <div>
+            <div>
+                <button
+                    className="
+                    mx-2 my-2 px-3 py-2
+                    border-2 border-gray-200 
+                    rounded-lg
+                    text-sm
+                    transition duration-150 ease-in-out 
+                    hover:border-gray-300"
+                    onClick={() => setIsCreate(true)}
+                >
+                    + Thêm Discount
+                </button>
+                <ModalCreate openCreate={[isCreate, setIsCreate]} reload={[reload, setReload]} />
+            </div>
+            {isLoading ? (
+                <div className="flex justify-center mt-6">
+                    <div
+                        className="w-16 h-16 border-8 border-green-400 rounded-full border-solid animate-spin"
+                        style={{ borderTop: "8px solid transparent" }}
+                    />
+                </div>
+            ) : (
+                <div className='pb-10'>
+                    <DiscountTable
+                        discountProps={[discount, setDiscount]}
+                        reload={[reload, setReload]}
+                    />
+                </div>
+            )}
+        </div>
+    )
 
 }
 
